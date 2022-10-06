364648d9452d0071ca6561de632936ddcddde08b
Helmi Pradita <helmipraditaa@gmail.com>
2022-10-06 21:55:23 +0700

    finish task1.js

---
+++ task1.js
@@ -0,0 +1,9 @@
+var name = 'Helmi Pradita'
+var email = 'helmipraditaa@gmail.com'
+var address = {
+    jalan: 'Jl Anggrek No 2',
+    kota: 'Mojokerto',
+    provinsi: 'Jawa Timur'
+}
+var isMerried = false
+var hobbies = ['Mendengarkan Musik', 'Main Game', 'Nonton Youtube']
