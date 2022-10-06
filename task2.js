bd111a702f135de4389a8c39c168991c417f2274
Helmi Pradita <helmipraditaa@gmail.com>
2022-10-06 22:07:28 +0700

    finish task2.js

---
+++ task2.js
@@ -0,0 +1,10 @@
+const printGenapGajil = 5
+
+for(let i = 1; i <= printGenapGajil; i++){
+    if(i%2 == 0){
+        console.log(`${i} merupakan bilangan genap`)    
+    } else {
+        console.log(`${i} merupakan bilangan ganjil`)
+    }
+    
+}
