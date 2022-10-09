7f1b65066c843da716c17e01d1bf464c6b1e0d02
Helmi Pradita <helmipraditaa@gmail.com>
2022-10-10 02:18:24 +0700

    change file js2-1.js

--- js2-1.js
+++ js2-1.js
@@ -1,56 +1,45 @@
 //split, reverse, join, foreach, map
 
-// //concat()
+// //concat() digunakan untuk mengabungkan 2 array  atau lebih, concat() tidak mengubah array yang ada tetapi membuat array baru
 // const array1 = ['a', 'b', 'c'];
 // const array2 = ['e', 'f', 'g'];
 // const array3 = array1.concat(array2);
 // console.log(array3);
 
-// //indexOf()
-// const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
-// console.log(beasts.indexOf('bison'));
-// console.log(beasts.indexOf('bison', 2));
-// console.log(beasts.indexOf('giraffe'));
+//indexOf() digunakan untuk mengetahui index dari array awal
+// const hewan = ['semut', 'bison', 'kelinci', 'bebek', 'bison'];
+// console.log(hewan.indexOf('bison'));
 
-// //lastIndexOf()
-// const animals = ['Dodo', 'Tiger', 'Penguin', 'Dodo'];
-// console.log(animals.lastIndexOf('Dodo'));
-// console.log(animals.lastIndexOf('Tiger'));
+// //lastIndexOf() digunakan untuk mengetahui index dari array akhir
+// const hewan = ['macan', 'singa', 'pinguin', 'singa'];
+// console.log(hewan.lastIndexOf('singa'));
 
-// //pop()
-// const plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato'];
-// console.log(plants.pop());
-// console.log(plants);
+// //pop() digunakan untuk menghapus element terakhir dari array
+// const sayuran = ['brokoli', 'wortel', 'cabai', 'tomat', 'sawi'];
+// console.log(sayuran.pop());
 
-// plants.pop();
-// console.log(plants);
+// //push() digunakan untuk menambahkan 1 atau beberapa element ke akhir array dan membuat array baru
+// const hewan = ['ayam', 'kambing', 'sapi'];
+// console.log(hewan.push('kerbau'));
+// console.log(hewan);
 
-// //push()
-// const animals = ['pigs', 'goats', 'sheep'];
-
-// const count = animals.push('cows');
-// console.log(count);
-// console.log(animals);
-
-// animals.push('chickens', 'cats', 'dogs');
-// console.log(animals);
-
-// //shift()
+// //shift() digunakan untuk menghapus element pertama pada array
 // const array1 = [1, 2, 3];
 // const firstElement = array1.shift();
 // console.log(array1);
 // console.log(firstElement);
 
-// //slice()
-// const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
-// console.log(animals.slice(2));
-// console.log(animals.slice(2, 4));
-// console.log(animals.slice(1, 5));
-// console.log(animals.slice(-2));
-// console.log(animals.slice(2, -1));
-// console.log(animals.slice());
+// //unshift() digunakan untuk menambahkan element pertama pada array
+// const array1 = [1, 2, 3];
+// console.log(array1.unshift(4, 5));
+// console.log(array1);
 
-// //sort()
+// //slice() digunakan untuk menyalin sebagian elemen array ke array baru
+// const hewan = ['semut', 'bison', 'kelinci', 'bebek', 'kerbau'];
+// console.log(hewan.slice(2));
+// console.log(hewan.slice(2, 4));
+
+// //sort() digunakan untuk mengurutkan element array da mengembalikan referensi ke array yang sama
 // const months = ['March', 'Jan', 'Feb', 'Dec'];
 // months.sort();
 // console.log(months);
@@ -58,15 +47,14 @@
 // array1.sort();
 // console.log(array1);
 
-// //toString()
-// const array1 = [1, 2, 'a', '1a'];
-// console.log(array1.toString());
+// //toUpperCase() digunakan untuk mengubah nilai dari string menjadi huruf besar semua
+// const kata = 'Hello world';
+// console.log(kata.toUpperCase());
 
-// //unshift()
-// const array1 = [1, 2, 3];
-// console.log(array1.unshift(4, 5));
-// console.log(array1);
+// //toLowerCase() digunakan untuk mengubah nilai dari string menjadi huruf kecil semua
+// const kata = 'Hello WORld';
+// console.log(kata.toLowerCase());
 
-//getMinutes()
-const birthday = new Date('March 13, 08 04:20');
-console.log(birthday.getMinutes());
+//getMinutes() digunakan untuk mengembalikan nilai menit pada tanggal yang ditentukan 
+// const birthday = new Date('March 13, 08 04:20');
+// console.log(birthday.getMinutes());
