ca269b28e79ef1754ed3052b07e630b5fcb4ef60
Helmi Pradita <helmipraditaa@gmail.com>
2022-10-12 10:25:53 +0700

    revisi js3-4.js

--- js3-2.js
+++ js3-2.js
@@ -1,17 +1,19 @@
 const getMonth = (callback) => {
     setTimeout(() => {
-        let error = false
+        let error = true
         let month = ['January', 'February', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember']
         if(!error) {
             callback(null, month)
         } else {
             callback(new Error("Sorry Data Not Found"), [])
-            console.log(error)
         }
     }, 3000)
 }
 
 const tampilkan = (err, bulan) => {
+    if(err){
+        console.log(err.message)
+    }
     let hasil = bulan.map(name => name)
     console.log(hasil)
 }
